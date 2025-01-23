3c9cf5acd69c9d0c593355c36fc307e4e8a84224
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2025-01-23 12:09:20 +0000

    refactor: Move helpers to separate file

--- main.ts
+++ main.ts
@@ -1,120 +1,15 @@
-import { extname, join } from "@std/path";
-import { emptyDirSync } from "@std/fs";
+import { emptyDirSync, walk } from "@std/fs";
 import { type Args, parseArgs } from "@std/cli/parse-args";
+import {
+  analyseDirectoryStructure,
+  findAudioFiles,
+  makeDirectoryStructure,
+} from "./utils.ts";
 
-// TODO move functions to utils folder
 // TODO explicit return
 // TODO import sort
 
-interface Tags {
-  BPM?: string;
-  TBPM?: string;
-  ARTIST?: string;
-  artist?: string;
-}
-
-interface FileInfo extends Deno.DirEntry {
-  filePath: string;
-  tags: Tags;
-}
-
-const audioExtensions = new Set([".flac", ".mp3"]);
-
 const folders = new Map<string, string[]>();
-
-// TODO docs and tests
-function getAudioFileTags(filePath: string): Tags | null {
-  const command = new Deno.Command("ffprobe", {
-    args: [
-      "-show_format",
-      "-print_format",
-      "json",
-      filePath,
-    ],
-  });
-  const { stdout, success } = command.outputSync();
-
-  if (success) {
-    // TODO Validate metadata with ZOD
-    const metaData = JSON.parse(new TextDecoder().decode(stdout));
-    const filteredTags = { ...metaData?.format?.tags };
-
-    // FLAC traktor tags
-    delete filteredTags.TRAKTOR4;
-
-    // MP3 traktor tags
-    Object.keys(filteredTags).forEach((key) => {
-      if (key.includes("id3v2_priv.")) {
-        delete filteredTags[key];
-      }
-    });
-
-    return filteredTags;
-  } else {
-    return null;
-  }
-}
-
-// TODO docs and tests
-export function findAudioFiles(path: string): FileInfo[] {
-  const files = [];
-
-  for (
-    const f of Deno.readDirSync(path)
-  ) {
-    const extension = extname(f.name);
-
-    if (audioExtensions.has(extension)) {
-      const filePath = join(path, f.name);
-      const tags = getAudioFileTags(filePath);
-
-      if (tags) {
-        files.push({ ...f, filePath, tags });
-      }
-    } else if (f.isDirectory) {
-      const recursivePath = join(path, f.name);
-
-      files.push(...findAudioFiles(recursivePath));
-    }
-  }
-
-  return files;
-}
-
-// TODO docs and tests
-function analyseDirectoryStructure(fileInfo: FileInfo[]): void {
-  fileInfo.forEach((i) => {
-    const bpmValue = i?.tags?.BPM || i?.tags?.TBPM;
-    const artistValue = i.tags.ARTIST || i.tags.artist;
-
-    // TODO log files that are missing a bpm or artist
-
-    if (bpmValue && artistValue) {
-      const parsedArtist = artistValue.toLowerCase().replace(
-        /[^a-zA-Z\s]/g,
-        "",
-      );
-
-      const nextValue = folders.has(bpmValue)
-        ? [...(folders.get(bpmValue) ?? []), parsedArtist]
-        : [parsedArtist];
-
-      folders.set(bpmValue, nextValue);
-    }
-  });
-}
-
-// TODO test + docs
-// TODO take output location as an argument
-function makeDirectoryStructure(): void {
-  for (const [bpm, values] of folders.entries()) {
-    Deno.mkdirSync(`./out/${bpm}`);
-
-    for (const folder of values.values()) {
-      Deno.mkdirSync(`./out/${bpm}/${folder}`);
-    }
-  }
-}
 
 function parseArguments(args: string[]): Args {
   const stringArgs = [
@@ -129,7 +24,7 @@ function parseArguments(args: string[]): Args {
   });
 }
 
-function main(inputArgs: string[]): void {
+async function main(inputArgs: string[]): Promise<void> {
   if (import.meta.main) {
     // TODO handle folders already existing
     emptyDirSync("./out");
@@ -137,16 +32,17 @@ function main(inputArgs: string[]): void {
     const { input } = parseArguments(inputArgs);
 
     const fileMetaData = findAudioFiles(input);
-    analyseDirectoryStructure(fileMetaData);
+    analyseDirectoryStructure(folders, fileMetaData);
 
-    makeDirectoryStructure();
+    makeDirectoryStructure(folders);
 
     // TODO decide on approach for moving audio file
 
-    for (const dirEntry of Deno.readDirSync("./out")) {
-      console.log("Basic listing:", dirEntry.name);
+    for await (const dirEntry of walk("./out")) {
+      console.log("Recursive walking:", dirEntry.name);
     }
   }
 }
 
 main(Deno.args);
+  
---
+++ utils.ts
@@ -0,0 +1,116 @@
+import { extname, join } from "@std/path";
+
+type Folders = Map<string, string[]>;
+
+interface Tags {
+  BPM?: string;
+  TBPM?: string;
+  ARTIST?: string;
+  artist?: string;
+}
+
+interface FileInfo extends Deno.DirEntry {
+  filePath: string;
+  tags: Tags;
+}
+
+const AUDIO_EXTENSIONS = new Set([".flac", ".mp3"]);
+
+// TODO docs and tests
+function getAudioFileTags(filePath: string): Tags | null {
+  const command = new Deno.Command("ffprobe", {
+    args: [
+      "-show_format",
+      "-print_format",
+      "json",
+      filePath,
+    ],
+  });
+  const { stdout, success } = command.outputSync();
+
+  if (success) {
+    // TODO Validate metadata with ZOD
+    const metaData = JSON.parse(new TextDecoder().decode(stdout));
+    const filteredTags = { ...metaData?.format?.tags };
+
+    // FLAC traktor tags
+    delete filteredTags.TRAKTOR4;
+
+    // MP3 traktor tags
+    Object.keys(filteredTags).forEach((key) => {
+      if (key.includes("id3v2_priv.")) {
+        delete filteredTags[key];
+      }
+    });
+
+    return filteredTags;
+  } else {
+    return null;
+  }
+}
+
+// TODO docs and tests
+function findAudioFiles(path: string): FileInfo[] {
+  const files: FileInfo[] = [];
+
+  for (
+    const f of Deno.readDirSync(path)
+  ) {
+    const extension = extname(f.name);
+
+    if (AUDIO_EXTENSIONS.has(extension)) {
+      const filePath = join(path, f.name);
+      const tags = getAudioFileTags(filePath);
+
+      if (tags) {
+        files.push({ ...f, filePath, tags });
+      }
+    } else if (f.isDirectory) {
+      const recursivePath = join(path, f.name);
+
+      files.push(...findAudioFiles(recursivePath));
+    }
+  }
+
+  return files;
+}
+
+// TODO docs and tests
+function analyseDirectoryStructure(
+  folders: Folders,
+  fileInfo: FileInfo[],
+): void {
+  fileInfo.forEach((i) => {
+    const bpmValue = i?.tags?.BPM || i?.tags?.TBPM;
+    const artistValue = i.tags.ARTIST || i.tags.artist;
+
+    // TODO log files that are missing a bpm or artist
+
+    if (bpmValue && artistValue) {
+      const parsedArtist = artistValue.toLowerCase().replace(
+        /[^a-zA-Z\s]/g,
+        "",
+      );
+
+      const nextValue = folders.has(bpmValue)
+        ? [...(folders.get(bpmValue) ?? []), parsedArtist]
+        : [parsedArtist];
+
+      folders.set(bpmValue, nextValue);
+    }
+  });
+}
+
+// TODO test + docs
+// TODO take output location as an argument
+function makeDirectoryStructure(folders: Folders): void {
+  for (const [bpm, values] of folders.entries()) {
+    Deno.mkdirSync(`./out/${bpm}`);
+
+    for (const folder of values.values()) {
+      Deno.mkdirSync(`./out/${bpm}/${folder}`);
+    }
+  }
+}
+
+export { analyseDirectoryStructure, findAudioFiles, makeDirectoryStructure };
