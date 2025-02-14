44dd63687f3f5d8cac44dc20429c38b89f346e33
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2025-02-14 11:55:47 +0000

    refactor: To is now a require argument
    Errors are now caught and argument validation has been improved.

--- main.ts
+++ main.ts
@@ -13,6 +13,7 @@ import {
   printMissingBPM,
 } from "./utils/logging.ts";
 import { validateToAndFromDirectories } from "./utils/errors.ts";
+import { red } from "@std/fmt/colors";
 
 // TODO add --help argument
 // TODO allow multiple from folders
@@ -36,46 +37,51 @@ function parseArguments(args: string[]): Args {
     "dry-run": "d",
   };
 
-  return parseArgs(args, {
+  const parsedArgs = parseArgs(args, {
     alias,
     boolean: booleanArgs,
     string: stringArgs,
-    // TODO decide on default location
-    default: { to: "./out" },
   });
+
+  validateToAndFromDirectories(parsedArgs.from, parsedArgs.to);
+
+  return parsedArgs;
 }
 
 function main(inputArgs: string[]): void {
   if (import.meta.main) {
-    const { from, to, ["dry-run"]: dryRun } = parseArguments(inputArgs);
+    try {
+      const { from, to, ["dry-run"]: dryRun } = parseArguments(inputArgs);
 
-    validateToAndFromDirectories(from, to);
+      // TODO handle folders already existing
+      emptyDirSync(to);
 
-    // TODO handle folders already existing
-    emptyDirSync(to);
+      const metadata = findAudioFiles(from);
+      const { missingBPM, missingArtist } = findMissingTags(metadata);
 
-    const metadata = findAudioFiles(from);
-    const { missingBPM, missingArtist } = findMissingTags(metadata);
+      if (!dryRun) {
+        copyAudioFiles(metadata, to);
+      }
 
-    if (!dryRun) {
-      copyAudioFiles(metadata, to);
+      // TODO handle all files have missing BPM or arist tags
+
+      printMissingBPM(missingBPM);
+      printMissingArtist(missingArtist);
+
+      const copiedFiles = metadata.filter((m) => m.tags.BPM && m.tags.artist);
+      const totalCopiedFiles = copiedFiles.length;
+      const foldersCreated = dryRun ? 0 : countDirectories(to);
+
+      dryRun
+        ? printDryRun(copiedFiles, to)
+        : printComplete(totalCopiedFiles, foldersCreated);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        console.log(red(`[${e.name}]`), red(e.message));
+      }
+
+      Deno.exit(1);
     }
-
-    // TODO handle all files have missing BPM or arist tags
-
-    console.log();
-
-    printMissingBPM(missingBPM);
-
-    printMissingArtist(missingArtist);
-
-    const copiedFiles = metadata.filter((m) => m.tags.BPM && m.tags.artist);
-    const totalCopiedFiles = copiedFiles.length;
-    const foldersCreated = dryRun ? 0 : countDirectories(to);
-
-    dryRun
-      ? printDryRun(copiedFiles, to)
-      : printComplete(totalCopiedFiles, foldersCreated);
   }
 }
 
--- utils/errors.ts
+++ utils/errors.ts
@@ -1,15 +1,34 @@
 import { existsSync } from "@std/fs";
-import { red } from "@std/fmt/colors";
 
-function validateToAndFromDirectories(from: string, to: string): void {
+class ValidationError extends Error {
+  constructor(...args: [string, ...(boolean | string)[]]) {
+    const [message, ...rest] = args;
+
+    const errorDetails = rest.filter(Boolean).join(", ");
+
+    super(message + errorDetails);
+    this.name = "ValidationError";
+  }
+}
+
+function validateToAndFromDirectories(from?: string, to?: string): void {
+  if (typeof from !== "string" || typeof to !== "string") {
+    throw new ValidationError(
+      "Missing required parameters: ",
+      !from && "from",
+      !to && "to",
+    );
+  }
+
   const invalidToPath = existsSync(to, { isFile: true });
   const invalidFromPath = existsSync(from, { isFile: true });
 
   if (invalidFromPath || invalidToPath) {
-    console.log(red("Error: invalid path"));
-    invalidFromPath && console.log(red("From must be a valid directory."));
-    invalidToPath && console.log(red("To must be a valid directory."));
-    Deno.exit(1);
+    throw new ValidationError(
+      "Invalid directory paths: ",
+      invalidFromPath && "from",
+      invalidToPath && "to",
+    );
   }
 }
 
